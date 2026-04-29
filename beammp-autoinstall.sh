b3ac4771f0dbd1bf4deb390e81d59f71a5019ab1
Gabriele <gabriele.r.00@proton.me>
2026-04-29 19:45:20 +0200

    Slight refactor of beammp-autoinstall.sh
    - Removal of error handling (since it's inside a container, it's basically impossible that it'll fail, plus the error would appear anyway)
    
    - Fused the container manager check and package manager check: if the container manager exists, no need to check for specific package managers and install them
    
    - After building the launcher, now "Dockerfile" will be deleted: it's basically clutter and if a user wants to see what's inside the file, it could just open the script with a text editor

--- beammp-autoinstall.sh
+++ beammp-autoinstall.sh
@@ -2,40 +2,35 @@
 
 set -e
 
-################## Package manager check ##################
+################## Container manager check | Package install if missing ##################
 if
-    (command -v apt || command -v pacman) > /dev/null 2>&1; then CONTAINER_MANAGER="docker"
-    elif
-        command -v emerge > /dev/null 2>&1; then CONTAINER_MANAGER="podman"
+  command -v podman > /dev/null 2>&1; then CONTAINER_MANAGER="podman"
+  elif
+  command -v docker > /dev/null 2>&1; then CONTAINER_MANAGER="docker"
     
-    else
-        exit 1
+  else
+    if
+      # Handle the most stupid problem I've seen: openjdk on gentoo installs /usr/bin/apt.
+      # It throws off the package-manager detection... this is ridicolous 
+      (command -v apt | command -v emerge) > /dev/null 2>&1; then sudo sh -c "$PORTAGE"
+      elif
+      command -v apt > /dev/null 2>&1; then sudo sh -c "$APT"
+      elif
+      command -v pacman > /dev/null 2>&1; then sudo sh -c "$PACMAN"
+      elif
+      command -v emerge > /dev/null 2>&1; then sudo sh -c "$PORTAGE"
+    fi
 fi
 ###########################################################
 ######################## Variables ########################
 APT="apt install podman-docker -y"
 PACMAN="pacman -S --needed --noconfirm docker"
-GENTOO="emerge -qU app-containers/podman"
+PORTAGE="emerge -qU app-containers/podman"
 
 BUILD="$CONTAINER_MANAGER build -t beammp-build:test ."
 RUN="$CONTAINER_MANAGER run --name beammp-container --replace beammp-build:test"
 COPY="$CONTAINER_MANAGER cp beammp-container:/home/BeamMP-Launcher ."
 STOP="$CONTAINER_MANAGER stop beammp-container"
-###########################################################
-##################### Package install #####################
-if
-    (command -v docker || command -v podman) > /dev/null 2>&1; then true
-    
-    elif
-        command -v apt > /dev/null 2>&1; then sudo sh -c "$APT"
-    elif
-        command -v pacman > /dev/null 2>&1; then sudo sh -c "$PACMAN"
-    elif
-        command -v emerge > /dev/null 2>&1; then sudo sh -c "$GENTOO"            
-    
-    else
-        exit 1
-fi
 ###########################################################
 ################### Dockerfile creation ###################
 tee Dockerfile > /dev/null << 'EOF'
@@ -44,6 +39,7 @@ FROM ubuntu:22.04
 
 # Let's install the dependencies using the package manager
 RUN apt-get update && apt-get install -y \
+    apt-utils \
     git \
     curl \
     cmake \
@@ -69,27 +65,13 @@ EOF
 ###########################################################
 ##### Build the container, execute it, copy file to . #####
     echo "Building container, cloning packages and compiling launcher..."
-sh -c "$BUILD" > /dev/null || sudo sh -c "$BUILD" > /dev/null || exit 2
+sh -c "$BUILD" > /dev/null || sudo sh -c "$BUILD" > /dev/null
     echo "Done! Now we mount the container and copy the launcher"
-sh -c "$RUN" > /dev/null || sudo sh -c "$RUN" > /dev/null || exit 3
-sh -c "$COPY" > /dev/null || sudo sh -c "$COPY" > /dev/null || exit 4
-sh -c "$STOP" > /dev/null || sudo sh -c "$STOP" > /dev/null || exit 5
+sh -c "$RUN" > /dev/null || sudo sh -c "$RUN" > /dev/null
+sh -c "$COPY" > /dev/null || sudo sh -c "$COPY" > /dev/null
+sh -c "$STOP" > /dev/null || sudo sh -c "$STOP" > /dev/null
     echo "Done! You will find the launcher in the folder you executed the script from!"
     echo "You can now close the terminal"
-###########################################################
-###################### Handle errors ######################
-errors()
-{
-    case $1 in
-        1) message="Valid package manager not found";;
-        2) message="Build failed inside docker container";;
-        3) message="Failed to run the container, file will not be copied";;
-        4) message="Failed to copy the built Launcher from the container... are permissions ok?";;
-        5) message="Failed to stop the container";;
-    esac
-  
-    echo "$message" >&2
-    exit "$1"
-}
+sh -c "rm Dockerfile"
 ###########################################################
 ###### Script made by pure spite, by @gabriele2000 ########
